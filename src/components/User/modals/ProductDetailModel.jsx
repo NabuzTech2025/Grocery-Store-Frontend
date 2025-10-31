aa620a8e2b3f097c95b63ce1368cfc935d7f0acd
AmitNabuz <amit.nabuztech@gmail.com>
2025-10-31 17:07:28 +0530

    add back button on productdetailmodel

--- src/components/User/modals/ProductDetailModel.jsx
+++ src/components/User/modals/ProductDetailModel.jsx
@@ -368,6 +368,26 @@ const ProductDetailModal = ({ product, isOpen, onClose }) => {
 
                     {/* Add to Cart Button */}
                     <div className="add-to-cart-section">
+                        <button
+                      type="button"
+                      className="btn-close d-md-none"
+                      onClick={onClose}
+                      aria-label="Close"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 16 16"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="1"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <polyline points="10 14 4 8 10 2"></polyline>
+                      </svg>
+                    </button>
                       <button
                         className="btn btn-primary btn-lg w-100"
                         onClick={handleAddToCart}
